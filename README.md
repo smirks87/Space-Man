4113e924bda207a8f497f77448297f88773923fc
smirks87 <spindy87@yahoo.com>
2022-09-15 19:18:19 -0700

    making progress

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # WORD SCRAMBLE
-First project 
+
+First project
 
 WORD SCRAMBLE is a game that test your spelling skills so when the game is started a random word is generated and the player has to guess letters until the word is spelled out.
-every time the player guesses a correct letter you they get to select another letter. when the player guesses wrong they lose a try. the player gets 5 tries and if the player uses all 5 tries the game is over. if the player guesses all of the right letters  you win the game
+every time the player guesses a correct letter they get to select another letter. when the player guesses wrong they lose a try. the player gets 5 tries and if the player uses all 5 tries the game is over. if the player guesses all of the right letters you win the game
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const word = ['reactor', 'engine', 'wings', 'booster', 'battery'];
 const body = document.querySelector('.body');
 
 const divWord = document.querySelector('.word');
-
+const wrongLetters = document.querySelector('.wrong-letters');
 const restart = document.getElementById('restart');
 const exit = document.getElementById('exit');
 const submit = document.getElementById('submit');
@@ -11,36 +11,44 @@ const submit = document.getElementById('submit');
 const correctGuesses = [];
 const wrongGuesses = [];
 
-let randomWord = '';
-
+let randomIndex = Math.floor(Math.random() * word.length);
+let randomWord = word[randomIndex];
+console.log(randomWord);
 
 function getRandomWord() {
-	
 	let randomIndex = Math.floor(Math.random() * word.length);
 	let randomWord = word[randomIndex];
 	console.log(randomWord);
 }
-// function compareLetter() {
-// 	randomWord.includes(form.value)
-// }
+
 function compareLetter(event) {
 	event.preventDefault();
-	let guessLetter = input.innerHTML;
+	let guessLetter = input.value;
 	console.log(guessLetter);
-	for (let i = 0; i < randomWord.length; i++) {
-		// console.log(randomWord);
-		if (randomWord[i] === guessLetter) {
-			console.log(true);
-			correctGuesses.push(guessLetter);
-			console.log(correctGuesses);
-		}
-		if (randomWord[i] == correctGuesses[i]) {
-			word.innerHTML = guessLetter;
-		}
+	console.log(randomWord);
+for (let i = 0; i < randomWord.length; i++) {
+	if (randomWord[i] === guessLetter) {
+		console.log(true);
+		correctGuesses.push(guessLetter);
+		console.log(correctGuesses);
+		divWord.innerHTML = correctGuesses;
+	} 
+}
+	if (!randomWord.includes(guessLetter)) {
+		console.log(false);
+		wrongGuesses.push(guessLetter);
+		console.log(wrongGuesses);
+
+		wrongLetters.innerHTML = wrongGuesses;
 	}
+	
+	
 }
 
-submit.addEventListener('submit', compareLetter);
+// }
+
+
+submit.addEventListener('click', compareLetter);
 
 // for (let i = 0; i < word.length; i++) {
 // 	let div = document.createElement('div');
