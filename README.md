6d8940e0cc827472ba738c74ebf86be3080530ef
smirks87 <spindy87@yahoo.com>
2022-09-18 19:06:10 -0700

    looking good

--- README.md
+++ README.md
@@ -3,4 +3,24 @@
 First project
 
 WORD SCRAMBLE is a game that test your spelling skills so when the game is started a random word is generated and the player has to guess letters until the word is spelled out.
-every time the player guesses a correct letter they get to select another letter. when the player guesses wrong they lose a try. the player gets 5 tries and if the player uses all 5 tries the game is over. if the player guesses all of the right letters you win the game
+every time the player guesses a correct letter they get to select another letter. when the player guesses wrong they keep guessing until they get it right. if the player guesses all of the right letters you they can click on the generate word button and a new word will generate for the player to try and guess. After the player gets all the words correct the game is over.
+
+
+TECHNOLOGY USED:
+html, javaScript, and css
+
+
+INSTALLATION:
+No installation required just have to click the link to play the game
+
+USER STORIES:
+
+- As a User, I want to have a button to generate a random word.
+- As a user, I want to have an input bar for guesses to be submitted into.
+- As a user, I want the right guessed letter to populate in the correct area in th correct order.
+- As a user, I want the wrong letters to populate in another box so the player knows they guessed a wrong letter.
+- As a user, I want the game to end after the player guesses the letters for all the words 
+
+WIRE FRAMES:
+
+
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const input = document.querySelector('.input');
 const word = ['reactor', 'engine', 'wings', 'booster', 'battery'];
 const body = document.querySelector('.body');
-
+const container = document.querySelector('.container');
+const endScreen = document.querySelector('.end-screen');
+let gameIsOver = false;
 let divWord = document.querySelector('.word');
 const wrongLetters = document.querySelector('.wrong-letters');
 const restart = document.getElementById('restart');
@@ -11,36 +13,48 @@ const submit = document.getElementById('submit');
 let correctGuesses = [];
 let wrongGuesses = [];
 let blankWord = [];
-let currentWord = word.length
+let currentWord = word.length;
 let randomIndex = Math.floor(Math.random() * currentWord);
 let randomWord = word[randomIndex];
-console.log(randomWord);
+
+
+function gameStart() {
+	if (gameIsOver === false) {
+		getRandomWord();
+		fillBlankWord();
+		compareLetter();
+	} else {
+		gameIsOver = true;
+		gameOver();
+	}
+}
+
 function getRandomWord() {
-	currentWord--
-	 randomIndex = Math.floor(Math.random() * currentWord);
-	 randomWord = word[currentWord];
-	console.log(randomWord);
-	wrongGuesses = []
-	correctGuesses = []
-	fillBlankWord()
-	console.log(blankWord)
-	divWord.innerHTML = blankWord
+	if (randomWord) {
+		currentWord--;
+		randomIndex = Math.floor(Math.random() * currentWord);
+		randomWord = word[currentWord];
+		wrongGuesses = [];
+		correctGuesses = [];
+		fillBlankWord();
+		divWord.innerHTML = blankWord;
+	}
 }
-
 // function compareWords() {
-	// 	blankWord.toString();
-	// 	console.log(blankWord);
-	// 	if ((blankWord = randomWord)) {
-		// 		// blankWord = []
-		// 	}
-		// }
-		
-		function fillBlankWord() {
-			blankWord = []
-			for (let i = 0; i < randomWord.length; i++) blankWord.push('_');
-			divWord.innerHTML = blankWord;
-}
+// 	blankWord.toString();
+// 	console.log(blankWord);
+// 	if ((blankWord = randomWord)) {
+// 		// blankWord = []
+// 	}
+// }
 
+function fillBlankWord() {
+	if (randomWord) {
+		blankWord = [];
+		for (let i = 0; i < randomWord.length; i++) blankWord.push('_');
+		divWord.innerHTML = blankWord;
+	}
+}
 
 function compareLetter(event) {
 	event.preventDefault();
@@ -49,41 +63,33 @@ function compareLetter(event) {
 	console.log(randomWord);
 	for (let i = 0; i < randomWord.length; i++) {
 		if (randomWord[i] === guessLetter) {
-			console.log(true);
 			correctGuesses.push(guessLetter);
-			console.log(correctGuesses);
 			blankWord.splice(i, 1, guessLetter);
 			divWord.innerHTML = blankWord;
-			console.log(blankWord);
+			
 		}
 		if (blankWord[i] === guessLetter) {
 		}
 	}
 	if (!randomWord.includes(guessLetter)) {
-		console.log(false);
 		wrongGuesses.push(guessLetter);
-		console.log(wrongGuesses);
-
 		wrongLetters.innerHTML = wrongGuesses;
 	}
 }
+function gameOver() {
+	if (currentWord < 0) {
+		gameIsOver = true;
+		container.style.display = 'none';
+		endScreen.style.display = 'inline';
+	}
+}
+gameOver();
 
-// function fillCorrectWord() {
-// 	for (let i = 0; i < randomWord.length; i++) {
-// 		if (correctGuesses[i] === randomWord[i]) {
-// 			divWord.innerHTML.replace('_', guessLetter);
-// 		}
-// 	}
-// }
-// fillCorrectWord()
+
 submit.addEventListener('click', compareLetter);
 
-// for (let i = 0; i < word.length; i++) {
-// 	let div = document.createElement('div');
-// 	div.classList.add('word');
-// 	// console.log(word);
-// }
+
 
 restart.addEventListener('click', getRandomWord);
 
-exit.addEventListener('click', function (event) {});
+
